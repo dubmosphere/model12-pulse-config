ba286cb9f313e46ce029c3ebed841a875c651f8e
Simon Häsler <simon.haesler@gmx.ch>
2026-02-13 01:06:05 +0100

    Change suspend/unsuspend orders for qjackctl startup/shutdown

--- qjackctl/scripts/on_startup.sh
+++ qjackctl/scripts/on_startup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-echo -n "pacmd suspend-sink alsa_output.usb-TASCAM_Model_12_no_serial_number-00.analog-stereo-output-12 1"
-pacmd suspend-sink alsa_output.usb-TASCAM_Model_12_no_serial_number-00.analog-stereo-output-12 1
-
 echo -n "pacmd suspend-sink model12_stereo_out 1"
 pacmd suspend-sink model12_stereo_out 1
 
-echo -n "pacmd suspend-source alsa_input.usb-TASCAM_Model_12_no_serial_number-00.analog-multichannel-input 1"
-pacmd suspend-source alsa_input.usb-TASCAM_Model_12_no_serial_number-00.analog-multichannel-input 1
+echo -n "pacmd suspend-sink alsa_output.usb-TASCAM_Model_12_no_serial_number-00.analog-stereo-output-12 1"
+pacmd suspend-sink alsa_output.usb-TASCAM_Model_12_no_serial_number-00.analog-stereo-output-12 1
 
 echo -n "pacmd suspend-source model12_ch1_mono 1"
 pacmd suspend-source model12_ch1_mono 1
@@ -35,3 +32,6 @@ pacmd suspend-source model12_ch910_stereo 1
 
 echo -n "pacmd suspend-source model12_main_stereo 1"
 pacmd suspend-source model12_main_stereo 1
+
+echo -n "pacmd suspend-source alsa_input.usb-TASCAM_Model_12_no_serial_number-00.analog-multichannel-input 1"
+pacmd suspend-source alsa_input.usb-TASCAM_Model_12_no_serial_number-00.analog-multichannel-input 1
